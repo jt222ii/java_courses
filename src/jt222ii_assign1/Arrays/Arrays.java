26dcc9cc80a8cd1b33ff7948dc96747c0e2c847f
jt222ii <jt222ii@student.lnu.se>
2016-08-29 19:02:07 +0200

    Made all the methods for Arrays. No actual code in the methods though

---
+++ src/jt222ii_assign1/Arrays/Arrays.java
@@ -0,0 +1,46 @@
+package jt222ii_assign1.Arrays;
+
+/**
+ * Created by Jonas on 2016-08-29.
+ */
+public class Arrays {
+    int sum(int[] arr)//return the sum of the array
+    {
+        return 0;
+    }
+
+    String toString(int[] arr)//returns a nice looking print out of the array content
+    {
+        return "";
+    }
+
+    int[] addN(int[] arr, int n)// that returns, an array where we have added the number n to all elements in the array arr. For example, in the case addN({1,2,3,4,5}, 2) the result is an array {3,4,5,6,7}.
+    {
+        return null;
+    }
+
+    int[] reverse(int[] arr) //that creates, and returns, a new array with all the elements in array arr but in reverse order. The array arr should be left unchanged.
+    {
+        return null;
+    }
+
+    void replaceAll(int[] arr, int old, int nw)// that replaces all occurences of the elemnt old with nw in arr.
+    {
+        return;
+    }
+
+    int[] sort(int[] arr)//that returns a new sorted array (least element first) with the same set of elements as in arr. The array arr should be left unchanged.
+    {
+        return null;
+    }
+
+    boolean hasSubsequence(int[] arr, int[] sub)//that returns true if the subsequence sub is a part of the array arr, otherwise false.
+    {
+        return false;
+    }
+
+    int[] absDif(int[] arr1, int[] arr2)// returns a new array that is the absolute difference between array arr1 and array arr2. That is result array dist(i) = |arr1(i) - arr2(i)|.
+    {
+        return null;
+    }
+}
