c63cdd3bdc778fbf0fd116701ce566c819ee5d41
jt222ii <jt222ii@student.lnu.se>
2016-08-30 18:54:42 +0200

    Exercise 7 done

--- src/jt222ii_assign1/Arrays/Arrays.java
+++ src/jt222ii_assign1/Arrays/Arrays.java
@@ -1,46 +1,104 @@
 package jt222ii_assign1.Arrays;
 
+import java.util.Collections;
 /**
  * Created by Jonas on 2016-08-29.
  */
 public class Arrays {
-    int sum(int[] arr)//return the sum of the array
+
+    public static int sum(int[] arr)//Method return the sum of the array
     {
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++)
+        {
+            sum += arr[i];
+        }
+        return sum;
     }
 
-    String toString(int[] arr)//returns a nice looking print out of the array content
+    public static String toString(int[] arr)//Method returns a nice looking print out of the array content
     {
-        return "";
+        String string = "";
+        for (int i = 0; i < arr.length; i++)
+        {
+            string += "\n[" + i + "]" + " = " + arr[i];
+        }
+        return string;
     }
 
-    int[] addN(int[] arr, int n)// that returns, an array where we have added the number n to all elements in the array arr. For example, in the case addN({1,2,3,4,5}, 2) the result is an array {3,4,5,6,7}.
+    public static int[] addN(int[] arr, int n)//Method that returns, an array where we have added the number n to all elements in the array arr. For example, in the case addN({1,2,3,4,5}, 2) the result is an array {3,4,5,6,7}.
     {
-        return null;
+        int[] newArray = arr.clone();
+        for (int i = 0; i < newArray.length; i++)
+        {
+            newArray[i] += n;
+        }
+        return newArray;
     }
 
-    int[] reverse(int[] arr) //that creates, and returns, a new array with all the elements in array arr but in reverse order. The array arr should be left unchanged.
+    public static int[] reverse(int[] arr) //Method that creates, and returns, a new array with all the elements in array arr but in reverse order. The array arr should be left unchanged.
     {
-        return null;
+        int[] newArray = new int[arr.length];
+        for (int i = 0; i < newArray.length; i++)
+        {
+            newArray[i] = arr[arr.length-1-i];
+        }
+        return newArray;
     }
 
-    void replaceAll(int[] arr, int old, int nw)// that replaces all occurences of the elemnt old with nw in arr.
+    public static void replaceAll(int[] arr, int old, int nw)//Method that replaces all occurences of the element old with nw in arr.
     {
-        return;
+        for (int i = 0; i < arr.length; i++)
+        {
+            if(arr[i] == old)
+            {
+                arr[i] = nw;
+            }
+        }
     }
 
-    int[] sort(int[] arr)//that returns a new sorted array (least element first) with the same set of elements as in arr. The array arr should be left unchanged.
+    public static int[] sort(int[] arr)//Method that returns a new sorted array (smallest element first) with the same set of elements as in arr. The array arr should be left unchanged.
     {
-        return null;
+        int[] newArray = arr.clone();
+        java.util.Arrays.sort(newArray);
+        return newArray;
     }
 
-    boolean hasSubsequence(int[] arr, int[] sub)//that returns true if the subsequence sub is a part of the array arr, otherwise false.
+    public static boolean hasSubsequence(int[] arr, int[] sub)//Method that returns true if the subsequence sub is a part of the array arr, otherwise false.
     {
+        if(arr.length < sub.length)
+        {
+            System.out.println("Can't look for a sequence larger than the array you're searching in!");
+            return false;
+        }
+        for (int i = 0; i < arr.length; i++)
+        {
+            for (int y = 0; y < sub.length; y++)//if a match is found keep looking if it is a sequence
+            {
+                if(sub[y] != arr[i+y])
+                {
+                    break;
+                }
+                else if(y == sub.length-1)//if the loop was not broken the sequence was found.
+                {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
-    int[] absDif(int[] arr1, int[] arr2)// returns a new array that is the absolute difference between array arr1 and array arr2. That is result array dist(i) = |arr1(i) - arr2(i)|.
+    public static int[] absDif(int[] arr1, int[] arr2)// returns a new array that is the absolute difference between array arr1 and array arr2. That is result array dist(i) = |arr1(i) - arr2(i)|.
     {
-        return null;
+        if(arr1.length != arr2.length) // in case they are different the method should throw an exception
+        {
+            throw new IllegalArgumentException("The arrays must be the same size!");
+        }
+        int[] newArray = new int[arr1.length];
+        for (int i = 0; i < newArray.length; i++)
+        {
+            newArray[i] = Math.abs(arr1[i]-arr2[i]);//set the value of newArray[i] to the absolute of arr1[i]-arr2[i]
+        }
+        return newArray;
     }
 }
---
+++ src/jt222ii_assign1/Arrays/ArraysMain.java
@@ -0,0 +1,42 @@
+package jt222ii_assign1.Arrays;
+
+/**
+ * Created by Jonas on 2016-08-30.
+ */
+public class ArraysMain {
+    public static void main (String args[])
+    {
+        int[] intArray = {1,2,3,4,5};
+        System.out.println("sum:");
+        System.out.println("sum of {1,2,3,4,5} = " + Arrays.sum(intArray));
+
+        System.out.println("toString:");
+        System.out.println("Array as string ="+Arrays.toString(intArray));
+
+        System.out.println("addN:");
+        System.out.println("{1,2,3,4,5} with 2 added to each element = "+Arrays.toString(Arrays.addN(intArray, 2)));
+
+
+        System.out.println("reverse:");
+        System.out.println("{1,2,3,4,5} reversed = "+Arrays.toString(Arrays.reverse(intArray)));
+
+        System.out.println("replaceAll:");
+        Arrays.replaceAll(intArray, 5, 40);
+        System.out.println("{1,2,3,4,5} with 40 replacing 5 = "+Arrays.toString(intArray));
+
+        int[] unsortedArray = {5,2,1,19};
+        System.out.println("sort:");
+        System.out.println("{5,2,1,19} sorted = "+Arrays.toString(Arrays.sort(unsortedArray)));
+
+        System.out.println("hasSubsequence(looking for {1,2,3,4} in {13,3,4,1,2,3,4}):");
+        int[] mainArray = {13,3,4,1,2,3,4};
+        int[] subArray = {1,2,3,4};
+        System.out.println(Arrays.hasSubsequence(mainArray, subArray));
+
+        System.out.println("absDif:");
+        int[] arr1 = {1,2,3,4,5};
+        int[] arr2 = {1,1,1,1,1};
+        System.out.println("using ({1,2,3,4,5}, {1,1,1,1,1}). Result should be {0,1,2,3,4}" + Arrays.toString(Arrays.absDif(arr1, arr2)));
+
+    }
+}
