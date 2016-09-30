198f4af055711332849d8702a8d841ae73a3a1af
jt222ii <jt222ii@student.lnu.se>
2016-09-30 17:14:23 +0200

    Tested the MyDfs with my own class

--- src/jt222ii_assign3/TestShit.java
+++ src/jt222ii_assign3/TestShit.java
@@ -7,14 +7,27 @@ public class TestShit {
     public static void main(String[] args) {
         MyGraph<String> graph = new MyGraph<>();
 
-        graph.addEdgeFor("a", "b");
+        /*graph.addEdgeFor("a", "b");
         graph.addEdgeFor("b", "c");
         graph.addEdgeFor("c", "d");
         graph.addEdgeFor("c", "e");
         System.out.println(graph.toString());
         MyGML<String> gml = new MyGML<>(graph);
 
-        gml.dumpGML();
+        gml.dumpGML();*/
 
+        graph.addEdgeFor("a", "c");
+        graph.addEdgeFor("a", "b");
+        graph.addEdgeFor("b", "d");
+        graph.addEdgeFor("c", "d");
+        graph.addEdgeFor("d", "e");
+        graph.addEdgeFor("d", "f");
+        graph.addEdgeFor("e", "g");
+        graph.addEdgeFor("e", "f");
+
+        MyDFS dfs = new MyDFS();
+        System.out.println(dfs.dfs(graph));
+        System.out.println(dfs.postOrder(graph));
+        System.out.println(dfs.topSort(graph));
     }
 }
