cbe1f0ff567e267a60d07ba20aa710ae50f3678c
jt222ii <jt222ii@student.lnu.se>
2016-10-07 18:18:48 +0200

    replaced the recursive function. The connected component is now faster and works in the benchmark

--- src/jt222ii_assign3/MyConnectedComponents.java
+++ src/jt222ii_assign3/MyConnectedComponents.java
@@ -4,7 +4,6 @@ import graphs.DirectedGraph;
 import graphs.Node;
 
 import java.util.*;
-import java.util.concurrent.ArrayBlockingQueue;
 
 /**
  * Created by jonastornfors on 2016-09-27.
@@ -24,72 +23,49 @@ public class MyConnectedComponents<E> implements graphs.ConnectedComponents<E> {
      * @author jonasl
      *
      */
-    /*private Set<Node<E>> visited = new HashSet<>();
+    private Set<Node<E>> visited = new HashSet<>();
     private LinkedList<Node<E>> nodes = new LinkedList<>();
-    private Collection<Collection<Node<E>>> mainList = new LinkedList<>();
-    private MyDFS<E> dfs = new MyDFS<>();*/
+    private MyDFS<E> dfs = new MyDFS<>();
+    private Map<Node<E>, Collection<Node<E>>> map = new HashMap<>();
     @Override
     public Collection<Collection<Node<E>>> computeComponents(DirectedGraph<E> dg) {
-        /*visited.clear();
-        nodes.clear();
-        mainList.clear();*/
-        Set<Node<E>> visited = new HashSet<>();
-        LinkedList<Node<E>> nodes = new LinkedList<>();
         Collection<Collection<Node<E>>> mainList = new LinkedList<>();
-        MyDFS<E> dfs = new MyDFS<>();
-        dg.iterator().forEachRemaining(nodes::add); //for each node in the graph add it to the list of nodes
+        LinkedList<LinkedList<Node<E>>> tmpMainList = new LinkedList<>();
+        visited.clear();
+        nodes.clear();
+        //dg.iterator().forEachRemaining(nodes::add); //for each node in the graph add it to the list of nodes
 
-        for (Node<E> n : nodes) //foreach node in the list of nodes
+        Iterator<Node<E>> nodesItr = dg.iterator();
+        while (nodesItr.hasNext()) //foreach node in the list of nodes
         {
-            List<Node<E>> tmpList = dfs.dfs(dg, n); //add the result of the dfs
+            Node<E> n = nodesItr.next();
+            if(!visited.contains(n)) {
+                List<Node<E>> tmpList = dfs.dfs(dg, n); //add the result of the dfs
 
-            List<Node<E>> nodesToAdd = new LinkedList<>(); //list of the nodes to add to the main list
+                LinkedList<Node<E>> nodesToAdd = new LinkedList<>(); //list of the nodes to add to the main list
 
-            for(Node<E> node : tmpList) //for each node in the temporary list we got from the dfs
-            {
-                if(!visited.contains(node))
+                boolean merged = false;
+                for (Node<E> node : tmpList) //for each node in the temporary list we got from the dfs
                 {
-                    visited.add(node); //add the node to the visited list
-                    nodesToAdd.add(node);//add the node to the list that will be added to the main list
-                    if(node.inDegree() > 0) // if the node has any predecessors
-                    {
-                        addPreds(nodesToAdd, node, visited); //add the predecessor to the nodesToAdd as well
+                    if (!visited.contains(node)) {
+                        visited.add(node); //add the node to the visited list
+                        nodesToAdd.add(node);//add the node to the list that will be added to the main list
+                        map.put(node, nodesToAdd);
+                    } else if (node != n) {
+                        merged = true;
+                        tmpMainList.get(tmpMainList.indexOf(map.get(node))).addAll(nodesToAdd);
                     }
 
                 }
-            }
-            if(nodesToAdd.size() > 0)//if the nodesToAdd list has anything to add... add it to the mainList
-            {
-                mainList.add(nodesToAdd);
-            }
-        }
-        return mainList;
-    }
 
-    /**
-     * Recursive function that adds all the predecessors and predecessors of predecessors to the list
-     * @param list
-     * @param n
-     * @return list
-     */
-    private List<Node<E>> addPreds(List<Node<E>> list, Node<E> n, Set<Node<E>> visited)
-    {
-        //Iterate through the predecessors
-        Iterator<Node<E>> itr = n.predsOf();
-        while(itr.hasNext())
-        {
-            Node<E> node = itr.next();
-            if(!visited.contains(node)) {
-                //add the node to the list and visited
-                list.add(node);
-                visited.add(node);
-                //if the node has predecessors. Add them as well
-                if (node.inDegree() > 0) {
-                    list = addPreds(list, node, visited);
+                if (nodesToAdd.size() > 0 && !merged)//if the nodesToAdd list has anything to add... add it to the mainList
+                {
+                    tmpMainList.add(nodesToAdd);
                 }
             }
         }
-        return list;
+        mainList.addAll(tmpMainList);
+        return mainList;
     }
 }
 
